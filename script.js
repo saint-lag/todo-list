1a6d0f7e4d599948c84ad38cb6b97a33967659a6
saint-lag <maiasena.gabriel@gmail.com>
2021-12-10 19:42:51 -0300

    LocalStorage: Saved Items Issue

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 // Create item
-const savedItems = JSON.parse(localStorage.getItem('savedItems'));
 const createButton = document.getElementById('criar-tarefa');
 const itemList = document.getElementById('lista-tarefas');
 const item = document.createElement('li');
 
-if (localStorage.getItem('sessionInitiated') === 'true') {
+if (
+  localStorage.sessionInitiated === 'true' &&
+  localStorage.savedItems !== 'undefined'
+) {
+  console.log('Session: TRUE');
+  var savedItems = JSON.parse(localStorage.savedItems);
   for (let savedItem of savedItems) {
-    itemList.appendChild(savedItem.cloneNode(true));
+    itemList.appendChild(savedItem);
+    console.log('Saved Item: ' + itemList);
   }
 } else {
+  console.log('Session: FALSE');
   localStorage.setItem('sessionInitiated', JSON.stringify(true));
   localStorage.setItem('savedItems', JSON.stringify([]));
+  var savedItems = [];
 }
 
 createButton.addEventListener('click', function () {
   item.style.listStyle = 'none';
   item.style.lineHeight = '20px';
+  item.className = 'item';
   item.innerText = document.getElementById('texto-tarefa').value;
   itemList.appendChild(item.cloneNode(true));
-  savedItems.push(item.innerHTML);
+  savedItems.push(item);
+  console.log('Item Saved: ' + item.innerText);
+
+  localStorage.savedItems = savedItems;
 });
